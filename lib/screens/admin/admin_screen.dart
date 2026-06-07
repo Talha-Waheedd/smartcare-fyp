4cc3d876fda4c833002b728deaa2f531f3ccaa7d
Talha Waheed <talhawaheed.bscskdkf22@iba-suk.edu.pk>
2026-06-07 22:26:30 +0500

    Update admin_screen.dart

--- lib/screens/admin/admin_screen.dart
+++ lib/screens/admin/admin_screen.dart
@@ -1,5 +1,3 @@
-// FILE: lib/screens/admin/admin_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
